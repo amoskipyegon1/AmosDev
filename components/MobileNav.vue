a762ffd2a255a28cc32d4c573002ea34c94b257e
Amos Kipyegon <amoskip@kabarak.ac.ke>
2023-01-03 09:46:05 +0300

    chore: Update on Clickable Links

---
+++ components/MobileNav.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul class="w-full flex flex-row justify-between items-center">
+        <li @click="$emit('ScrollFunction', 'header')">
+            <div class="w-7 h-7 text-slate-500 hover:text-slate-300 transition-colors duration-200">
+                <IconsHome />
+            </div>
+        </li>
+        <li @click="$emit('ScrollFunction', 'skills')">
+            <div class="w-7 h-7 text-slate-500 hover:text-slate-300 transition-colors duration-200">
+                <IconsSkills />
+            </div>
+        </li>
+        <li @click="$emit('ScrollFunction', 'experience')">
+            <div class="w-7 h-7 text-slate-500 hover:text-slate-300 transition-colors duration-200">
+                <IconsExperience />
+            </div>
+        </li>
+        <li @click="$emit('ScrollFunction', 'projects')">
+            <div class="w-7 h-7 text-slate-500 hover:text-slate-300 transition-colors duration-200">
+                <IconsProjects />
+            </div>
+        </li>
+    </ul>
+</template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,6 +2,7 @@
 const itemref = ref<HTMLDivElement>();
 const loader = ref<boolean>(false);
 
+const openSocialLinks = ref<boolean>(false);
 onMounted(() => {
     const itemObs = new IntersectionObserver(
         (entry) => {
@@ -22,8 +23,7 @@ onMounted(() => {
 <template>
     <nav
         ref="itemref"
-        class="z-50 grid grid-cols-2 sm:grid-cols-3 gap-x-2 w-full px-4 md:px-8 lg:px-20 py-3 md:py-2.5 lg:py-4 sticky top-0 bg-tate border-b border-neutral-800 transition duration-500"
-        :class="loader ? 'opacity-1 translate-y-0' : 'opacity-0 -translate-y-1/3'"
+        class="z-50 sticky top-0 grid grid-cols-2 sm:grid-cols-3 gap-x-2 w-full px-4 md:px-8 lg:px-20 py-2 md:py-2.5 lg:py-3 bg-tate border-b border-neutral-800 transition duration-500"
     >
         <ul class="links hidden sm:flex flex-row items-center gap-x-8">
             <li @click="$emit('ScrollFunction', 'header')">home</li>
@@ -38,7 +38,57 @@ onMounted(() => {
                 Kipyegon</span
             >
         </div>
-        <div class="flex flex-row gap-x-8 items-center justify-end">
+        <div class="flex sm:hidden justify-end items-center px-4 relative">
+            <div
+                @click="openSocialLinks = !openSocialLinks"
+                class="w-6 h-6 text-slate-500 hover:text-slate-300 transition-colors duration-200"
+            >
+                <IconsConnect />
+            </div>
+            <Transition name="drop-down">
+                <div
+                    v-if="openSocialLinks"
+                    class="drop-down fixed z-50 top-0 left-0 right-0 px-3 py-6 bg-tate shadow-lg flex justify-center items-center flex-col gap-y-8"
+                >
+                    <div class="close absolute top-4 right-8">
+                        <div
+                            @click="openSocialLinks = !openSocialLinks"
+                            class="w-7 h-7 text-gray-500 hover:text-gray-50 cursor-pointer transition-colors duration-200"
+                        >
+                            <IconsClose />
+                        </div>
+                    </div>
+                    <NuxtLink to="https://twitter.com/_ditto1" target="_blank" class="flex flex-col gap-y-1 items-center justify-center">
+                        <div class="w-5 h-5 text-gray-400 hover:text-gray-50 cursor-pointer transition-colors duration-200">
+                            <IconsTwitter />
+                        </div>
+                        <small class="text-sm capitalize tracking-wide">twitter</small>
+                    </NuxtLink>
+                    <NuxtLink
+                        to="https://www.linkedin.com/in/amos-kipyegon/"
+                        target="_blank"
+                        class="flex flex-col gap-y-1 items-center justify-center"
+                    >
+                        <div class="w-5 h-5 text-gray-400 hover:text-gray-50 cursor-pointer transition-colors duration-200">
+                            <IconsLinkedin />
+                        </div>
+                        <small class="text-sm capitalize tracking-wide">linkedin</small>
+                    </NuxtLink>
+                    <NuxtLink to="https://github.com/Amos-Ditto" target="_blank" class="flex flex-col gap-y-1 items-center justify-center">
+                        <div class="w-5 h-5 text-gray-400 hover:text-gray-50 cursor-pointer transition-colors duration-200">
+                            <IconsGithub />
+                        </div>
+                        <small class="text-sm capitalize tracking-wide">github</small>
+                    </NuxtLink>
+                </div>
+            </Transition>
+            <div
+                v-if="openSocialLinks"
+                class="toggling-bg fixed top-0 bottom-0 left-0 right-0 card-tate z-20"
+                @click="openSocialLinks = !openSocialLinks"
+            ></div>
+        </div>
+        <div class="hidden sm:flex flex-row gap-x-8 items-center justify-end">
             <NuxtLink to="https://twitter.com/_ditto1" target="_blank">
                 <div class="w-5 h-5 text-gray-400 hover:text-gray-50 cursor-pointer transition-colors duration-200">
                     <IconsTwitter />
@@ -61,5 +111,15 @@ onMounted(() => {
 <style scoped>
 .links li {
     @apply text-base font-light tracking-wide capitalize hover:text-gray-50 cursor-pointer transition-colors duration-200;
+}
+
+.drop-down-enter-from,
+.drop-down-leave-to {
+    @apply -translate-y-full opacity-0;
+}
+
+.drop-down-enter-active,
+.drop-down-leave-active {
+    @apply transition duration-500;
 }
 </style>
