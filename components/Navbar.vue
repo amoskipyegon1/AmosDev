9663ac989ddd1f2f512515e7bacdefbb828055a7
Amos Kipyegon <amoskip@kabarak.ac.ke>
2023-03-20 09:09:41 +0300

    chore: Home page design update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,41 +1,43 @@
 <template>
     <nav
-        class="w-full flex flex-row items-center justify-between py-4 px-48 fixed top-0 left-0 right-0 bg-white backdrop-saturate-200 backdrop-blur-2xl bg-opacity-80 shadow-sm z-40"
+        class="w-full flex flex-row items-center justify-center fixed top-0 left-0 right-0 bg-white backdrop-saturate-200 backdrop-blur-2xl bg-opacity-80 shadow-sm z-40"
     >
-        <div class="logo flex flex-row items-center">
-            <button class="text-xl font-bold text-deep-color tracking-tight">Amos.dev</button>
-        </div>
-        <ul class="nav-links flex flex-row items-start gap-x-8 text-deep-color">
-            <li
-                class="flex items-center px-1 py-2 font-normal text-base transition duration-200 text-current cursor-pointer"
-            >
-                Home
-            </li>
-            <li
-                class="flex items-center px-1 py-2 font-normal text-base transition duration-200 text-current cursor-pointer"
-            >
-                About
-            </li>
-            <li
-                class="flex items-center px-1 py-2 font-normal text-base transition duration-200 text-current cursor-pointer"
-            >
-                Projects
-            </li>
-            <li
-                class="flex items-center px-1 py-2 font-normal text-base transition duration-200 text-current cursor-pointer"
-            >
-                Blogs
-            </li>
-        </ul>
-        <div class="flex flex-row items-center gap-x-6">
-            <button>
-                <Icon name="mdi:github" class="text-deep-color text-3xl" />
-            </button>
-            <button
-                class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-sm shadow-blue-500/20 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] flex items-center"
-            >
-                Let's Talk
-            </button>
+        <div class="container flex flex-row items-center justify-between py-4">
+            <div class="logo flex flex-row items-center">
+                <button class="text-xl font-bold text-deep-color tracking-tight">Amos.dev</button>
+            </div>
+            <ul class="nav-links flex flex-row items-start gap-x-8 text-deep-color">
+                <li
+                    class="flex items-center px-1 py-2 font-normal text-base transition duration-200 text-current cursor-pointer"
+                >
+                    Home
+                </li>
+                <li
+                    class="flex items-center px-1 py-2 font-normal text-base transition duration-200 text-current cursor-pointer"
+                >
+                    About
+                </li>
+                <li
+                    class="flex items-center px-1 py-2 font-normal text-base transition duration-200 text-current cursor-pointer"
+                >
+                    Projects
+                </li>
+                <li
+                    class="flex items-center px-1 py-2 font-normal text-base transition duration-200 text-current cursor-pointer"
+                >
+                    Blogs
+                </li>
+            </ul>
+            <div class="flex flex-row items-center gap-x-6">
+                <button>
+                    <Icon name="mdi:github" class="text-deep-color text-3xl" />
+                </button>
+                <button
+                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-sm shadow-blue-500/20 hover:shadow-lg hover:shadow-blue-500/40 active:opacity-[0.85] flex items-center"
+                >
+                    Let's Talk
+                </button>
+            </div>
         </div>
     </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,28 @@
 <template>
     <div class="w-full flex flex-col">
-        <div
-            class="hero w-screen h-screen min-h-screen relative z-0 bg-[url('/images/bg-header.webp')] bg-cover bg-no-repeat bg-center"
-        >
-            <!-- <div class="absolute -z-10 top-0 bottom-0 right-0 left-0">
-                <NuxtImg src="/images/bg-header.webp" class="w-full h-full" />
-            </div> -->
+        <div class="hero relative z-0 flex items-start justify-center">
+            <!-- bg-[url('/images/bg-header.webp')] bg-cover bg-no-repeat bg-center -->
+            <div class="container flex flex-col items-center py-20 lg:py-32">
+                <div class="flex flex-col items-end justify-center">
+                    <NuxtImg src="/imgs/upper.jpg" class="rounded-full w-[200px] h-[200px]" />
+                </div>
+                <div class="flex flex-col items-center justify-center gap-y-1 mt-10">
+                    <h3 class="text-5xl font-black tracking-wide leading-tight">Full-Stack developer</h3>
+                    <p class="font-light text-xl">
+                        Hi, I'm Amos Kipyegon. A Full-stack Developer based in Nairobi, Kenya.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="w-full flex justify-center">
+            <div class="container bg-white shadow-sm rounded py-4 px-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam repudiandae possimus architecto autem
+                nemo quisquam, sequi sed. Repudiandae quae magni odit accusantium officia accusamus, consequatur
+                laboriosam suscipit architecto voluptate harum!
+            </div>
         </div>
     </div>
+    <!-- I enjoy building solid
+    and user-friendly web applications using my experience in interface design, TypeScript and
+    VueJS. Also optimized backends and database models, Django, NodeJS and RestApi/GraphQL. -->
 </template>
